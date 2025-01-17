61989bf80d89a9b17223d49e8afb9d9a2c7a0364
emciv02 <vcmedeiros21@gmail.com>
2025-01-17 16:21:33 +0100

    remove db

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,8 +1,8 @@
 // import bcrypt from "bcrypt";
+// import { db } from "@vercel/postgres";
 // import { invoices, customers, revenue, users } from "../lib/placeholder-data";
 
 // const client = await db.connect();
-// const connectionPool = require('./db');
 
 // async function seedUsers() {
 //   await connectionPool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
